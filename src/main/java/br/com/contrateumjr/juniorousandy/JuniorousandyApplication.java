24110c8df02361fc6de2c3ac81588f1430f1b2bb
Bruno Barros <contato4code@gmail.com>
2022-10-02 01:06:01 -0300

    init API

---
+++ src/main/java/br/com/contrateumjr/juniorousandy/JuniorousandyApplication.java
@@ -0,0 +1,13 @@
+package br.com.contrateumjr.juniorousandy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuniorousandyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuniorousandyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/contrateumjr/juniorousandy/JuniorousandyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.contrateumjr.juniorousandy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuniorousandyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
