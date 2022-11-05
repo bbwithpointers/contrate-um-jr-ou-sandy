b0be6d5a5343e0c8cce6cc461e19bfd732cf84c9
Bruno Barros <contato4code@gmail.com>
2022-11-05 00:12:09 -0300

    Removido dependencia do mysql, alterado para mongodb. Inicio da criação das classes com TDD

---
+++ src/main/java/br/com/contrateumjr/juniorousandy/model/Usuario.java
@@ -0,0 +1,4 @@
+package br.com.contrateumjr.juniorousandy.model;
+
+public class Usuario {
+}
--- src/test/java/br/com/contrateumjr/juniorousandy/JuniorousandyApplicationTests.java
+++ src/test/java/br/com/contrateumjr/juniorousandy/JuniorousandyApplicationTests.java
@@ -1,13 +1,19 @@
 package br.com.contrateumjr.juniorousandy;
 
+import br.com.contrateumjr.juniorousandy.model.Usuario;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class JuniorousandyApplicationTests {
 
     @Test
-    void contextLoads() {
-    }
+   public void criaUsuarioTest() {
+       Usuario usuario = new Usuario();
+        assertNotNull(usuario);
+   }
 
 }
